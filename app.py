073578b5c6d5d7a269014b171b552d59ab453697
Giovann Wah <giovannwah@Giovanns-MacBook-Air.local>
2018-06-15 22:36:00 -0400

    Fixes to api functions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, jsonify, request
+from flask import Flask, Response, jsonify, request, abort
 from werkzeug.datastructures import Headers
 from flask_sqlalchemy import SQLAlchemy
 import functools
@@ -16,13 +16,18 @@ def getScores():
     '''
     Get all scores
     ''' 
+    scores = HighScores.query.all()
+    if len(scores) > 0:
+        scores.sort(reverse=True)
+    else:
+        return jsonify([])
     ret = [{'rank': x.rank,
             'game_matches': x.game_matches,
             'game_time': x.game_time,
             'game_efficiency': x.game_efficiency,
             'user': x.user,
             'date': x.date,
-            'location': x.location} for x in HighScores.query.all()]
+            'location': x.location} for x in scores]
     return jsonify(ret)
 
 @app.route("/last-score", methods=['GET'])
@@ -31,13 +36,16 @@ def getLastScore():
     Get the score with the lowest rank in the database.
     '''
     x = HighScores.getLast()
-    return jsonify({'rank': x.rank,
-            'game_matches': x.game_matches,
-            'game_time': x.game_time,
-            'game_efficiency': x.game_efficiency,
-            'user': x.user,
-            'date': x.date,
-            'location': x.location})
+    if not x is None: 
+        return jsonify({'rank': x.rank,
+                'game_matches': x.game_matches,
+                'game_time': x.game_time,
+                'game_efficiency': x.game_efficiency,
+                'user': x.user,
+                'date': x.date,
+                'location': x.location})
+    else: 
+        return jsonify({})
 
 @app.route("/first-score", methods=['GET'])
 def getFirstScore():
@@ -45,75 +53,60 @@ def getFirstScore():
     Get the score with the highest rank in the database.
     '''
     x = HighScores.getFirst()
-    return jsonify({'rank': x.rank,
-            'game_matches': x.game_matches,
-            'game_time': x.game_time,
-            'game_efficiency': x.game_efficiency,
-            'user': x.user,
-            'date': x.date,
-            'location': x.location})
+    if not x is None: 
+        return jsonify({'rank': x.rank,
+                'game_matches': x.game_matches,
+                'game_time': x.game_time,
+                'game_efficiency': x.game_efficiency,
+                'user': x.user,
+                'date': x.date,
+                'location': x.location})
+    else: 
+        return jsonify({})
 
 @app.route("/add-score", methods=['POST'])
 def addScore():
     '''
     Get new score, add to current list of scores, sort by rank, then remove extra items
     '''
-    scores = HighScores.query.all()
-    # add new score and sort scores based in ordering
-    data = request.get_json(force=True)
-    new_score = HighScores(rank=int(data['rank']), game_matches=int(data['game_matches']), game_time=float(data['game_time']),
-                           game_efficiency=float(data['game_efficiency']), user=str(data['user']), date=str(data['date']), 
-                           location=str(data['location']))
-    if len(scores) == 0:
-        new_score.rank = 1
-        db.session.add(new_score)
-        db.session.commit()
-    else:    
-        print(f'SCORE LENGTH: {len(scores)}')
-        for sco in scores:
-            print(sco)
-        #clear current high scores table
-        #HighScores.query.delete(scores)
-        #db.session.query(HighScores).delete()
-        #db.session.commit()
-        #print('After DELETE')
-        #print(f'scores length: {len(scores)}')
-         
-        scores.append(new_score)
-        print('Before Sort...')
-        for sco in scores: 
-            print(sco)
-        scores.sort(reverse=True)
-        # assign new ranks after sorting scores
-        for x in range(1, len(scores)+1):
-            scores[x-1].rank = x
-        print('After Sort and rank assignment...')
+    try:
+        scores = HighScores.query.all()
+        # add new score and sort scores based in ordering
+        data = request.get_json(force=True)
+        new_score = HighScores(rank=int(data['rank']), game_matches=int(data['game_matches']), game_time=float(data['game_time']),
+                               game_efficiency=float(data['game_efficiency']), user=str(data['user']), date=str(data['date']), 
+                               location=str(data['location']))
+        if len(scores) == 0:
+            new_score.rank = 1
+            db.session.add(new_score)
+            db.session.commit()
+        else:    
+            db.session.add(new_score)
+            scores = HighScores.query.all()
+            scores.sort(reverse=True)
+            # assign new ranks after sorting scores
+            for x in range(1, len(scores)+1):
+                scores[x-1].rank = x
+            # Truncate extra scores
+            if len(scores) > MAX_STORED_SCORES:
+                for score in scores:
+                    if score.rank > MAX_STORED_SCORES:
+                        db.session.delete(score)
+           
+            db.session.commit()
             
-        for sco in scores: 
-            print(sco)
-        # Truncate extra scores
-        if len(scores) > MAX_STORED_SCORES:
-            for score in scores:
-                if score.rank > MAX_STORED_SCORES:
-                    db.session.remove(score)
+    except Exception as e:
+        print(f'An unexpected error occured: {e}')
+        abort(500)        
+    else: 
+        h = Headers()
+        h.add('Content-Type', 'application/json')
+        return Response(mimetype='application/json', response='{"message":"OK","status":"200"}', status='OK', headers=h)
         
-        print('After Sort and rank assignment...')
-            
-        for sco in scores: 
-            print(sco)
-        #save scores 
-        db.session.bulk_save_objects(scores)   
-        #for score in scores: 
-        #    db.session.add(score)
-        db.session.commit()
-        
-    h = Headers()
-    h.add('Content-Type', 'application/json')
-    return Response(mimetype='application/json', response='{"message":"OK","status":"200"}', status='OK', headers=h)
-
 @functools.total_ordering
 class HighScores(db.Model):
-    rank = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
+    rank = db.Column(db.Integer, nullable = False)
     game_matches = db.Column(db.Integer, nullable = False)
     game_time = db.Column(db.Float, nullable = False)
     game_efficiency = db.Column(db.Float, nullable = False)
@@ -160,7 +153,11 @@ class HighScores(db.Model):
         Return the lowest score.
         '''
         scores = HighScores.query.all()
-        return scores[-1]
+        scores.sort(reverse=True)
+        if len(scores) > 0:
+            return scores[-1]
+        else:
+            return None
         
 if __name__ == "__main__":
     db.create_all()
