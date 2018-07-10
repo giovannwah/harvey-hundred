67d9c7ad1d1363c464075c2bac52b4f42b2a153b
Giovann Wah <giovannwah@s172-20-103-h169.paws.uga.edu>
2018-07-10 16:58:52 -0400

    Moved images and implemented base-level game functionality

--- frontend/harvey_hundreds/src/App.js
+++ frontend/harvey_hundreds/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import GameApi from './api'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import ReactTransitionGroup from 'react-addons-transition-group' 
 
-/** 
- * Class that renders all informational popups in the game.
-*/
 class Popup extends Component {
   render() {
     return (
@@ -14,22 +13,32 @@ class Popup extends Component {
   }
 }
 
-/** 
- * Class that represents a single card that can be flipped and matched with other cards.
-*/
 class Card extends Component {
   render() {
-    return (
-      <button className="card" onClick={this.props.onClick}>
-        {this.props.value}
-      </button>
-    );
+	if (!this.props.inPlay) {
+		const img_url = "images/cash.jpg";
+		return (
+				<button className="card" style={{backgroundImage: `url(${img_url})`, backgroundSize: '100%', backgroundRepeat: 'no-repeat'}}></button>	
+		);
+	}
+	else {
+		if (this.props.flip) {
+			const img_url = this.props.img;
+		    return (
+		    	<button className="card" onClick={this.props.onClick} style={{backgroundImage: `url(${img_url})`, backgroundSize: '100%', backgroundRepeat: 'no-repeat'}}></button>
+		    );
+		}
+		else {
+			return (
+				<button className="card" onClick={this.props.onClick}>
+				{this.props.value}
+				</button>
+			);
+		}
+	}
   }
 }
 
-/** 
- * All game logic 
-*/
 class App extends Component {
   constructor(props) {
     super(props);
@@ -41,42 +50,50 @@ class App extends Component {
     this.timeDecrement = this.timeDecrement.bind(this);
     this.moneyAmount = this.moneyAmount.bind(this);
     this.calculateEfficiency = this.calculateEfficiency.bind(this);
+    this.getImageForCardID = this.getImageForCardID.bind(this);
+    this.isMatch = this.isMatch.bind(this);
+    this.canClickAll = this.canClickAll.bind(this);
   }
 
+  /*
+   * Return an object representing the initial game state
+   */
   initGameState() {
     return {
       matches: 0,
       timeLeft: 60.0,
       gameRunning: false,
-
       cardFlips: 0,
       cardSelected: null,
       pairs: this.makePairs(),
       cards: [
-        {id: 1, flipped: false, inPlay: true},
-        {id: 2, flipped: false, inPlay: true},
-        {id: 3, flipped: false, inPlay: true},
-        {id: 4, flipped: false, inPlay: true},
-        {id: 5, flipped: false, inPlay: true},
-        {id: 6, flipped: false, inPlay: true},
-        {id: 7, flipped: false, inPlay: true},
-        {id: 8, flipped: false, inPlay: true},
-        {id: 9, flipped: false, inPlay: true},
-        {id: 10, flipped: false, inPlay: true},
-        {id: 11, flipped: false, inPlay: true},
-        {id: 12, flipped: false, inPlay: true},
-        {id: 13, flipped: false, inPlay: true},
-        {id: 14, flipped: false, inPlay: true},
-        {id: 15, flipped: false, inPlay: true},
-        {id: 16, flipped: false, inPlay: true},
-        {id: 17, flipped: false, inPlay: true},
-        {id: 18, flipped: false, inPlay: true},
-        {id: 19, flipped: false, inPlay: true},
-        {id: 20, flipped: false, inPlay: true}
+        {id: 1, flipped: false, inPlay: true, canClick: true},
+        {id: 2, flipped: false, inPlay: true, canClick: true},
+        {id: 3, flipped: false, inPlay: true, canClick: true},
+        {id: 4, flipped: false, inPlay: true, canClick: true},
+        {id: 5, flipped: false, inPlay: true, canClick: true},
+        {id: 6, flipped: false, inPlay: true, canClick: true},
+        {id: 7, flipped: false, inPlay: true, canClick: true},
+        {id: 8, flipped: false, inPlay: true, canClick: true},
+        {id: 9, flipped: false, inPlay: true, canClick: true},
+        {id: 10, flipped: false, inPlay: true, canClick: true},
+        {id: 11, flipped: false, inPlay: true, canClick: true},
+        {id: 12, flipped: false, inPlay: true, canClick: true},
+        {id: 13, flipped: false, inPlay: true, canClick: true},
+        {id: 14, flipped: false, inPlay: true, canClick: true},
+        {id: 15, flipped: false, inPlay: true, canClick: true},
+        {id: 16, flipped: false, inPlay: true, canClick: true},
+        {id: 17, flipped: false, inPlay: true, canClick: true},
+        {id: 18, flipped: false, inPlay: true, canClick: true},
+        {id: 19, flipped: false, inPlay: true, canClick: true},
+        {id: 20, flipped: false, inPlay: true, canClick: true}
       ]
     };
   }
 
+  /*
+   * Returns a score representing the user's playing efficiency 
+   */
   calculateEfficiency() {
     const matches = this.state.matches;
     const flips = this.state.cardFlips % 2 === 0 ? this.state.cardFlips : this.state.cardFlips - 1; //only an even number of card flips can count as a valid set of attempts.
@@ -86,14 +103,15 @@ class App extends Component {
     return standard_efficiency/user_efficiency;
   }
 
-  /**
-   * Initialize matching pairs of cards
+  /*
+   * Create and return random pairs of cards, each pair assigned a random image
    */
   makePairs() {
     var arr = Array.from({length: 20}, (v, k) => k+1); //card ids
     var arr2 = Array.from({length: 60}, (v, k) => k+1); //image ids
     var ret = [];
     
+    //returns a random number between 0 and max, excluding ex
     var rInt = (max, ex) => {
       if (ex === -1) return Math.floor(Math.random() * Math.floor(max));
       else {
@@ -104,12 +122,14 @@ class App extends Component {
         return ret;
       }
     };
+    //return two distinct random numbers greater than 0 and less than len
     var randoms = (len) => {
       var r1 = rInt(len, -1); 
       var r2 = rInt(len, r1);
       return {r1: r1, r2: r2};
     };
 
+    //copy array, excluding ex1 and ex2 values
     var arrCpy = (ar, ex1, ex2) => {
       var ret = [];
       var i;
@@ -121,6 +141,7 @@ class App extends Component {
       return ret;
     };
 
+    //create random pairs of ids with random
     while (arr.length > 0) {
       let inds = randoms(arr.length);
       var pair = {id1: arr[inds.r1], id2: arr[inds.r2]};
@@ -134,7 +155,53 @@ class App extends Component {
     console.log(ret);
     return ret;
   }
+  
+  /*
+   * Get the relative url of the image assigned to this card id
+   */
+  getImageForCardID(id) {
+	  var i;
+	  for (i = 0; i < this.state.pairs.length; i++) {
+		  if (this.state.pairs[i].id1 === id || this.state.pairs[i].id2 === id) {
+			  return this.state.pairs[i].img;
+		  }
+	  }
+	  return null;
+  }
+  
+  /*
+   * Returns true if card with id1 and card with id2 are a pair
+   */
+  isMatch(id1, id2) {
+	  var ret = false;
+	  var i = 0;
+	  for (i = 0; i < this.state.pairs.length; i++) {
+		  if (this.state.pairs[i].id1 === id1 && this.state.pairs[i].id2 === id2) {
+			  ret = true;
+			  break;
+		  }
+		  else if (this.state.pairs[i].id1 === id2 && this.state.pairs[i].id2 === id1) {
+			  ret = true;
+			  break;
+		  }
+	  }
+	  return ret;
+  }
 
+  /*
+   * Sets all cards' "canClick" attribute to either true or false
+   */
+  canClickAll(b) {
+	  let cards = this.state.cards;
+	  let i;
+	  for (i = 0; i < cards.length; i++) {
+		  cards[i].canClick = b;
+	  }
+	  this.setState({
+		 cards: cards 
+	  });
+  }
+  
   handleLastScore (r) {
     
   }
@@ -157,21 +224,77 @@ class App extends Component {
 
   handleApiError () {}
 
+  /*
+   * Render a Card with the given props
+   */
   renderCard(id) {
     return (
-      <Card onClick={() => this.handleClick(id)} value={id}/>
+      <Card onClick={() => this.handleClick(id)} value={id} flip={this.state.cards[id-1].flipped} img={this.getImageForCardID(id)} inPlay={this.state.cards[id-1].inPlay} />
     );
   }
 
+  /*
+   * Handle clicks on cards
+   */
   handleClick(id) {
-     if (this.state.cardSelected === null) {
-       let cardz = this.state.cards;
-       cardz[id-1].flipped = true;
-       this.setState({
-         cardSelected: this.state.cards[id-1],
-         cards: cardz
-       });
-     }
+	 //only handle click if the card/game is in a state to accept a click 
+	 if (this.state.gameRunning && this.state.cards[id-1].inPlay && this.state.cards[id-1].canClick) {
+		 let cards = this.state.cards;
+	     cards[id-1].flipped = true;
+	     
+	     //there is no card selected currently
+	     if (!this.state.cardSelected) {
+	       cards[id-1].canClick = false;
+	       this.setState((prevState, props) => ({
+	         cardSelected: prevState.cards[id-1],
+	         cardFlips: prevState.cardFlips + 1,
+	         cards: cards
+	       }));
+	     }
+	     //there is already a card selected
+	     else {
+	    	//set flipped state for this card
+	    	this.setState((prevState, props) => ({
+	    		cards: cards,
+	    		cardFlips: prevState.cardFlips + 1
+	    	}));
+	    	//prevent clicks and pause for a moment
+	    	this.canClickAll(false);
+	    	let context = this;
+	    	setTimeout(function() {
+	    		const id1 = context.state.cardSelected.id;
+	        	const id2 = id;
+	        	const match = context.isMatch(id1, id2);
+	        	//check if clicked card matches previously clicked card, remove the cards from play
+	        	if (match) {
+	        		cards[id1-1].inPlay = false;
+	        		cards[id2-1].inPlay = false;
+	        	}
+	        	//if no match, just flip the cards back.
+	        	else {
+	        		cards[id1-1].flipped = false;
+	        		cards[id2-1].flipped = false;
+	        	}
+	        	//set state and enable clicks, or end game if there are no more matches
+	        	let matchesLeft = function () {
+	        		var i;
+	        		for (i = 0; i < context.state.cards.length; i++) {
+	        			if (context.state.cards[i].inPlay === true) {
+	        				return true;
+	        			}
+	        		}
+	        		return false;
+	        	}
+	        	context.setState((prevState, props) => ({
+	        		gameRunning: matchesLeft() ? true : false,
+        			matches: match ? prevState.matches + 1 : prevState.matches,
+	        		cards: cards,
+        			cardSelected: null
+        		}));
+	        	context.canClickAll(true);
+	    	}, 850);
+	     }
+	 }
   }
 
   moneyAmount() {
@@ -187,9 +310,9 @@ class App extends Component {
     const running = this.state.gameRunning;
     const time = this.state.timeLeft;
     if (running) {
-      this.setState({
-        timeLeft: this.state.timeLeft - 0.1
-      });
+      this.setState((prevState, props) => ({
+        timeLeft: prevState.timeLeft - 0.1
+      }));
     }
     if (time < 0.1) {
       this.setState({
@@ -213,37 +336,38 @@ class App extends Component {
       return (
         <div className="game_container">
             <div className="info_container">
-              <p>{this.moneyAmount()}</p>
-              <p>{parseFloat(Math.round((this.state.timeLeft)*100) / 100).toFixed(1)}</p>
+              <p>Card Flips: {this.state.cardFlips}</p>
+              <p>Money Earned: {this.moneyAmount()}</p>
+              <p>Time Left (s): {parseFloat(Math.round((this.state.timeLeft)*100) / 100).toFixed(1)}</p>
             </div>
-            <div className="cards_container">
-              <div className="card_row">
+            <div className="container">
+              <div className="row">
                 <div className="card_container">{this.renderCard(1)}</div>
                 <div className="card_container">{this.renderCard(2)}</div>            
                 <div className="card_container">{this.renderCard(3)}</div>   
                 <div className="card_container">{this.renderCard(4)}</div>            
                 <div className="card_container">{this.renderCard(5)}</div>         
               </div>
-              <div className="card_row">
+              <div className="row">
                 <div className="card_container">{this.renderCard(6)}</div>
                 <div className="card_container">{this.renderCard(7)}</div>            
                 <div className="card_container">{this.renderCard(8)}</div>   
                 <div className="card_container">{this.renderCard(9)}</div>            
                 <div className="card_container">{this.renderCard(10)}</div>  
               </div>
-              <div className="card_row">
+              <div className="row">
                 <div className="card_container">{this.renderCard(11)}</div>
                 <div className="card_container">{this.renderCard(12)}</div>            
                 <div className="card_container">{this.renderCard(13)}</div>   
                 <div className="card_container">{this.renderCard(14)}</div>            
                 <div className="card_container">{this.renderCard(15)}</div>  
               </div>
-              <div className="card_row">
+              <div className="row">
                 <div className="card_container">{this.renderCard(16)}</div>
                 <div className="card_container">{this.renderCard(17)}</div>            
                 <div className="card_container">{this.renderCard(18)}</div>   
-                <div className="card_container">{this.renderCard(19)}</div>            
-                <div className="card_container">{this.renderCard(20)}</div>  
+                <div className="card_container">{this.renderCard(19)}</div>
+                <div className="card_container">{this.renderCard(20)}</div>
               </div>
             </div>
             <button id="start" onClick={() => this.startGame()}>
